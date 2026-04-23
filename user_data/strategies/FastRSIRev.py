83368f29dd6c489a6380c954107a7c98f64dbef8
auddk25 <auddk25@users.noreply.github.com>
2026-04-23 13:49:52 +0800

    r18: StochRev stoch_k<0.10(mandatory evolve); FastRSIRev exit RSI>55

--- user_data/strategies/FastRSIRev.py
+++ user_data/strategies/FastRSIRev.py
@@ -57,6 +57,6 @@ class FastRSIRev(IStrategy):
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        exit_cond = (dataframe["rsi_fast"] > 50) | (dataframe["close"] > dataframe["bb_middle"])
+        exit_cond = (dataframe["rsi_fast"] > 55) | (dataframe["close"] > dataframe["bb_middle"])
         dataframe.loc[exit_cond, "exit_long"] = 1
         return dataframe
