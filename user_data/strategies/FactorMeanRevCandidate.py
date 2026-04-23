4406816dc1b715bca15b499a0d61f1460551d176
auddk25 <auddk25@users.noreply.github.com>
2026-04-23 22:28:22 +0800

    Revert "r83: FactorMeanRevCandidate BB entry multiplier 0.997->0.998 (slightly wider entry)"
    This reverts commit 6189b117348b84125e2f3e8b11c8be82417e0072.

--- user_data/strategies/FactorMeanRevCandidate.py
+++ user_data/strategies/FactorMeanRevCandidate.py
@@ -91,7 +91,7 @@ class FactorMeanRevCandidate(IStrategy):
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         base_condition = dataframe["close"] > dataframe["ema200"]
         base_condition &= dataframe["adx"] > 19
-        base_condition &= dataframe["close"] < dataframe["bb_lower"] * 0.998
+        base_condition &= dataframe["close"] < dataframe["bb_lower"] * 0.997
 
         if self._uses_factor_gate(metadata):
             stoch_factor = dataframe.get("stoch_factor_k", dataframe["stoch_k"])
