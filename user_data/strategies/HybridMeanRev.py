c90b01087a8a21a59e5d6479bf87c839bdece7ba
auddk25 <auddk25@users.noreply.github.com>
2026-04-23 14:32:43 +0800

    r49: HybridMeanRev ROI 0.010

--- user_data/strategies/HybridMeanRev.py
+++ user_data/strategies/HybridMeanRev.py
@@ -22,7 +22,7 @@ class HybridMeanRev(IStrategy):
     timeframe = "1h"
     can_short = False
 
-    minimal_roi = {"0": 0.008}
+    minimal_roi = {"0": 0.010}
     stoploss = -0.08
 
     trailing_stop = False
